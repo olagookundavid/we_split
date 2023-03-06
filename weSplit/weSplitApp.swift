073107ff4104031779f78b3196d8c9d21ee77d8e
David Olagookun <erijesudo@gmail.com>
2023-03-06 20:50:20 +0100

    Initial Commit

---
+++ weSplit/weSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  weSplitApp.swift
+//  weSplit
+//
+//  Created by David OH on 06/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct weSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
