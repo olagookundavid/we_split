bbf44ade03c907484726d34f961c50b948fff6f0
David Olagookun <erijesudo@gmail.com>
2023-03-08 15:17:57 +0100

    finished project

--- weSplit/ContentView.swift
+++ weSplit/ContentView.swift
@@ -8,14 +8,96 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var checkamt = 0.0
+      @State private var numberOfPeople = 2
+      @State private var tipPercentage = 20
+    @FocusState private var isFocused: Bool //focus and unfocus keyboard
+    
+      let percentages = [10,15,20,25,0]
+    
+    var totalPerPerson: Double{
+        let peopleCount = Double(numberOfPeople + 2)
+        let tipSelection = Double(tipPercentage)
+        
+        let tipValue = checkamt/100 * tipSelection
+        let grandTotal = checkamt + tipValue
+        return grandTotal/peopleCount
+    }
+    
+    
+    var totalCheckAmt : Double{
+        let tipSelection = Double(tipPercentage)
+        
+        let tipValue = checkamt/100 * tipSelection
+        return  checkamt + tipValue
+    }
+    
+    
+    var totalTipAmt : Double{
+        let tipSelection = Double(tipPercentage)
+        
+        return checkamt/100 * tipSelection
+    }
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView{
+            Form{
+                Section{
+                    TextField("Amount", value: $checkamt, format: .currency(code:Locale.current.currency?.identifier ?? "USD"))
+                        .keyboardType(.decimalPad)
+                    
+                    Picker("Number Of People",selection: $numberOfPeople){
+                        ForEach(2..<100){
+                            Text("\($0) people")
+                        }
+                    }}
+                    
+                    Section{
+                        Picker("Tips ",selection: $tipPercentage){
+                            ForEach(percentages,id: \.self){
+                                Text($0 ,format:.percent)
+                            }
+                        }.pickerStyle(.segmented)
+                    }header: {
+                        
+                            Text("How much tip do you want to leave")
+                    }
+                    Section{
+                        Text(totalPerPerson, format: .currency(code:Locale.current.currency?.identifier ?? "USD"))
+                            .keyboardType(.decimalPad)
+                            .focused($isFocused)
+                    }header: {
+                        
+                        Text("Amount per person")
+                }
+                
+                Section{
+                    Text(totalCheckAmt, format: .currency(code:Locale.current.currency?.identifier ?? "USD"))
+                        .keyboardType(.decimalPad)
+                        .focused($isFocused)
+                }header: {
+                    
+                    Text("Total Check Amount")
+            }
+                Section{
+                    Text(totalTipAmt, format: .currency(code:Locale.current.currency?.identifier ?? "USD")).keyboardType(.decimalPad).focused($isFocused)
+                }header: {
+                    
+                    Text("Total Tip Amount")
+            }
+                
+                
+            }
+            .navigationTitle("We split").toolbar{//attach done button to keyboard
+                ToolbarItemGroup(placement: .keyboard){Spacer()
+                    Button("Done"){
+                        isFocused = false
+                    }
+                }
+            }
         }
-        .padding()
+      
     }
 }
 
